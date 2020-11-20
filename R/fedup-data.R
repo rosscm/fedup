6167fa600a992e7e0d65eeded1affa4bfb878218
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2020-11-20 16:44:16 -0500

    data description

--- R/fedup-data.R
+++ R/fedup-data.R
@@ -1,9 +1,27 @@
 #' Example list of yeast GO pathways obtained from a GMT file.
+#' Script to generate example pathway list
+#' system.file("pathwaysGMT.R", package = "FEDUP")
 #' @docType data
 #' @name pathwaysGMT
 NULL
 
 #' Example list of yeast SAFE terms obtained from a XLSX file.
+#' Script to generate example pathway list
+#' system.file("pathwaysXLSX.R", package = "FEDUP")
 #' @docType data
 #' @name pathwaysXLSX
+NULL
+
+#' Example vector of yeast genes to use as test set for enrichment.
+#' Script to generate example test gene vector
+#' system.file("genes.R", package = "FEDUP")
+#' @docType data
+#' @name testGene
+NULL
+
+#' Example vector of yeast genes to use as background set for enrichment.
+#' Script to generate example background gene vector
+#' system.file("genes.R", package = "FEDUP")
+#' @docType data
+#' @name backgroundGene
 NULL

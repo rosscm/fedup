65665421d9a190abe672edec101d0c1bbd626639
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-01-29 18:19:23 -0500

    data descriptions
    Former-commit-id: 5ebc188727a4cd20269837f3d8e80f5b973c4df0

---
+++ R/data.R
@@ -0,0 +1,40 @@
+#' Named list of human pathway annotations obtained from a GMT file.
+#'
+#' Raw GMT file is available from
+#' \url{http://download.baderlab.org/EM_Genesets/November_17_2020/Human/symbol/Pathways/Human_Reactome_November_17_2020_symbol.gmt}
+#'
+#' Raw data location
+#' system.file("extdata", "Human_Reactome_November_17_2020_symbol.gmt", package = "FEDUP")
+#'
+#' Script to prepare data
+#' system.file("data-raw", "pathwaysGMT.R", package = "FEDUP")
+#'
+#' @format a named list of 8080 vectors
+"pathwaysGMT"
+
+#' Example list of yeast SAFE terms obtained from a XLSX file.
+#'
+#' Raw data location
+#' system.file("extdata", "SAFE_terms.xlsx", package = "FEDUP")
+#'
+#' Script to prepare data
+#' system.file("data-raw", "pathwaysXLSX.R", package = "FEDUP")
+#'
+#' @format a named list of 30 vectors
+"pathwaysXLSX"
+
+#' Example vector of human genes to use as test set for enrichment.
+#'
+#' Script to prepare data
+#' system.file("data-raw", "genes.R", package = "FEDUP")
+#'
+#' @format a character vector with 396 elements (gene IDs)
+"testGene"
+
+#' Example vector of human genes to use as background set for enrichment.
+#'
+#' Script to generate data
+#' system.file("data-raw", "genes.R", package = "FEDUP")
+#'
+#' @format a character vector with 15965 elements (gene IDs)
+"backgroundGene"

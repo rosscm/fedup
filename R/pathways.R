8fbacccaee452081683df133b6ffceda0a936438
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-01-29 18:21:30 -0500

    changed func name from writePathways()->readPathways(); added new writePathways() func
    Former-commit-id: 5e5785e718838f93078b0810502a6f29b0b08755

--- R/pathways.R
+++ R/pathways.R
@@ -1,4 +1,4 @@
-#' Returns a list of pathways from a file.
+#' Returns a list of pathways from a variety of file formats.
 #' Currently supports the following file format: GMT, TXT, XLSX
 #' @param pathway_file (char) path to file with pathway annotations
 #' @param header (logical) whether pathway_file has a header (default = TRUE)
@@ -10,31 +10,32 @@
 #'  (default = 1)
 #' @param MAX_GENE (integer) maximum number of genes to be considered in a pathway
 #'  (default = Inf)
-#' @param GO_class (char) if GO terms are present in 'pathway_file', which class(es)
-#'  should be used (i.e., BP, MF, CC) (default = NULL)
+#' @param GO_class (char) if GO terms are present in 'pathway_file', which
+#'  class(es) should be subsetted (i.e., BP, MF, CC)
+#'  (default = NULL)
 #' @return a list of vectors with pathway annotations
 #' @examples
-#' pathways <- writePathways(
-#'  system.file("extdata", "Human_GOBP_AllPathways_no_GO_iea_November_17_2020_symbol.gmt", package = "FEDUP"),
-#'  MIN_GENE = 10, MAX_GENE = 500,
+#' pathways <- readPathways(
+#'  system.file("extdata", "Human_Reactome_November_17_2020_symbol.gmt",
+#'  package = "FEDUP"), MIN_GENE = 10, MAX_GENE = 500
 #')
-#' pathways <- writePathways(
+#' pathways <- readPathways(
 #'  system.file("extdata", "SAFE_terms.xlsx", package = "FEDUP"),
 #'  header = TRUE, pathway_col = "Enriched.GO.names", gene_col = "Gene.ID",
 #'  MIN_GENE = 10, MAX_GENE = 500
 #')
-#' @import openxlsx
-#' @import tibble
-#' @importFrom stats aggregate
+#' @importFrom openxlsx read.xlsx
+#' @importFrom tibble deframe
+#' @importFrom stats aggregate na.omit
 #' @importFrom utils head read.delim tail
 #' @export
-writePathways <- function(pathway_file,
-                          header = TRUE,
-                          pathway_col,
-                          gene_col,
-                          MIN_GENE = 1L,
-                          MAX_GENE = Inf,
-                          GO_class = NULL) {
+readPathways <- function(pathway_file,
+                         header = TRUE,
+                         pathway_col,
+                         gene_col,
+                         MIN_GENE = 1L,
+                         MAX_GENE = Inf,
+                         GO_class = NULL) {
   # Vector of supported file types
   supported_types <- c("gmt", "txt", "xlsx")
   file_type <- sub(".*\\.", "", pathway_file) # grab pathway_file extension
@@ -69,6 +70,12 @@ writePathways <- function(pathway_file,
         pathway = pathway_in[,pathway_col],
         gene = pathway_in[,gene_col]
       )
+
+      # Ensure no empty entries present
+      pathway_df[which(pathway_df$gene == ""), "gene"] <- NA
+      pathway_df <- na.omit(pathway_df)
+
+      # Transform to list
       pathway_df <- aggregate(gene ~ pathway, data = pathway_df, FUN = paste)
       #pathway_df$gene <- strsplit(pathway_df$gene, "[[:punct:]] ", perl = TRUE)
       pathways <- deframe(pathway_df)
@@ -109,6 +116,31 @@ writePathways <- function(pathway_file,
                  "arguments specified."))
     }
   }
-  # Return subsetted list of pathways
+  # Return named list of pathways
   return(pathways_sub)
+}
+
+#' Writes a set of pathways (list of vectors) to a GMT file
+#' @param pathways (list) named list of vectors
+#' @param gmt_file (char) name of output GMT file
+#' @return GMT formatted file. Rows represent gene sets. Columns represent:
+#' \itemize{
+#'    \item gene set name;
+#'    \item description;
+#'    \item a list of tab-delimited genes
+#' }
+#' @examples
+#' data(pathwaysXLSX)
+#' writePathways(pathwaysXLSX, tempfile("pathwaysXLSX", fileext = ".gmt"))
+#' @importFrom data.table fwrite
+#' @export
+writePathways <- function(pathways, gmt_file) {
+    # Prepare pathway data table for GMT output
+    tab <- data.table(
+      pathway = names(pathways),
+      description = gsub("\\%.*", "", names(pathways)),
+      genes = unlist(lapply(pathways, paste, collapse = "\t"))
+    )
+    # Write out file
+    fwrite(tab, file = gmt_file, sep = "\t", col.names = FALSE, quote = FALSE)
 }

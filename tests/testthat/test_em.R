331bc090d7b9d05417b2e16c4402158035f0f427
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-02-05 18:29:23 -0500

    femap test file
    Former-commit-id: cff97d02d341ac79617a34515497ecda095b6b8c

---
+++ tests/testthat/test_em.R
@@ -0,0 +1,16 @@
+context("Enrichment map")
+
+test_that("Test that writeFemap works", {
+  data(testGene)
+  data(backgroundGene)
+  data(pathwaysGMT)
+  fedup_res <- runFedup(testGene, backgroundGene, pathwaysGMT)
+  results_file <- tempfile("fedup_res", fileext = ".txt")
+  writeFemap(fedup_res, results_file)
+  femap_res <- read.delim(results_file)
+
+  expect_equal(nrow(fedup_res), nrow(femap_res))
+  expect_true("phenotype" %in% colnames(femap_res))
+  expect_true(fedup_res[1,"enrichment"] == "Enriched" && femap_res[1,"phenotype"] == 1)
+  expect_true(fedup_res[1436,"enrichment"] == "Depleted" && femap_res[1436,"phenotype"] == -1)
+})

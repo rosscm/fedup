459b301e29c7ff974c976415f88661414bf4c63f
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-02-05 18:29:37 -0500

    fdr->qvalue; pval->pvalue
    Former-commit-id: 668bb25efafc7ce178769680215a1398fbef651e

--- tests/testthat/test_enrichment.R
+++ tests/testthat/test_enrichment.R
@@ -7,11 +7,11 @@ test_that("Test that FEDUP analysis works", {
   fedup_res <- runFedup(testGene, backgroundGene, pathwaysGMT)
 
   expect_equal(fedup_res[1, real_pathway_frac], 100.00000)
-  expect_equal(fedup_res[1, fdr], 1.567426e-186)
+  expect_equal(fedup_res[1, qvalue], 1.567426e-186)
   expect_true("NKX2-5" %in% fedup_res[,real_pathway_gene][[1]])
   expect_true(!"OR11A1" %in% fedup_res[,real_pathway_gene][[1]])
   expect_equal(fedup_res[1436, real_pathway_frac], 0.0000000)
-  expect_equal(fedup_res[1436, fdr], 1.000000e+00)
+  expect_equal(fedup_res[1436, qvalue], 1.000000e+00)
   expect_false("NKX2-5" %in% fedup_res[,real_pathway_gene][[1436]])
   expect_true(!"OR11A1" %in% fedup_res[,real_pathway_gene][[1436]])
 })

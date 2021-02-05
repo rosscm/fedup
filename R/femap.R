cb2d668c4bb1a9d90660a0c7e7e5872017f26ed5
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-02-05 18:31:14 -0500

    applying autoannotate and network layout to enrichment map; added argument to export network to file
    Former-commit-id: c3126130693e727347f870ce753af7b847dffd5c

--- R/femap.R
+++ R/femap.R
@@ -1,17 +1,18 @@
-#' Writes an enrichment dataset file for use in Cytoscape EnrichmentMap
-#' @param df (data.frame) table with FEDUP enrichment results
-#'  (see ?runFedup() for column descriptions)
-#' @param results_file (char) name of output results file
+#' Writes an enrichment dataset file for use in Cytoscape EnrichmentMap.
+#'
+#' @param df (data.frame) table with FEDUP enrichment results.
+#'  (see runFedup() for column descriptions)
+#' @param results_file (char) name of output results file.
 #' @return table of gene enrichment and depletion results formatted as a
 #' 'Generic results file'. Rows represent tested pathways. Columns represent:
 #' \itemize{
-#'    \item geneset -- gene-set ID (must match the gene-set ID in the GMT file);
+#'    \item pathway -- pathway ID (must match pathway IDs in the GMT file provided to plotFemap());
 #'    \item description -- gene-set name or description;
 #'    \item pvalue -- enrichment pvalue;
-#'    \item qvalue -- FDR correction value;
+#'    \item qvalue -- BH-corrected pvalue;
 #'    \item phenotype -- +1 or -1, to identify enrichment in up- and down-regulation,
-#'             or, more in general in either of the two phenotypes being compared
-#'             in the two-class analysis (+1 maps to red, -1 maps to blue)
+#'            or, more in general in either of the two phenotypes being compared
+#'            in the two-class analysis (+1 maps to red, -1 maps to blue)
 #' }
 #' @examples
 #' data(testGene)
@@ -24,99 +25,99 @@
 #' @importFrom dplyr select mutate %>%
 #' @export
 writeFemap <- function(df, results_file) {
+    df_em <- df %>%
+        select("pathway", "pvalue", "qvalue", "enrichment") %>%
+        mutate("description" = gsub("\\%.*", "", df$pathway)) %>%
+        mutate("enrichment" = ifelse(df$enrichment == "Enriched", "1", "-1")) %>%
+        select("pathway", "description", "pvalue", "qvalue", "enrichment") %>%
+        `colnames<-` (c("pathway", "description", "pvalue", "qvalue", "phenotype"))
 
-	# Format FEDUP results to generate generic EnrichmentMap in Cytoscape
-  df_em <- df %>%
-		select("pathway", "pvalue", "fdr", "enrichment") %>%
-		mutate("description" = gsub("\\%.*", "", df$pathway)) %>%
-    mutate("enrichment" = ifelse(df$enrichment == "enriched", "1", "-1")) %>%
-		select("pathway", "description", "pvalue", "fdr", "enrichment") %>%
-		`colnames<-` (c("geneset", "description", "pvalue", "qvalue", "phenotype"))
-
-  # Write out file
-	fwrite(df_em, results_file, sep = "\t", col.names = TRUE, quote = FALSE)
+    fwrite(df_em, results_file, sep = "\t", col.names = TRUE, quote = FALSE)
+    message("Wrote Cytoscape-formatted FEDUP results file to ", results_file)
 }
 
-#' Plots an EnrichmentMap (EM) in Cytoscape to visualize enriched and depleted gene sets.
-#' @param gmt_file (char) path to GMT file (must be absolute path)
-#' @param results_file (char) path to file with enrichment results (must be absolute path)
-#' @param pvalue (numeric) pvalue cutoff (value between 0 and 1). Gene set
-#'  nodes with a p-value lower than the given value will not be included
-#' 	in the network. (default = 1)
-#' @param qvalue (numeric) FDR qvalue cutoff (value between 0 and 1). Gene set
-#' 	nodes with a p-value lower than the given value will not be included
-#' 	in the network. (default = 1)
-#' @param net_name (char) name for network in Cytoscape
-#' @return filename of image to which EM is exported. Also side effect of
-#'  plotting the EM in an open session of Cytoscape.
+#' Draws a network representation of overlaps among enriched and depleted
+#' pathways using EnrichmentMap (EM) in Cytoscape.
+#'
+#' @param gmt_file (char) path to GMT file (must be an absolute path).
+#' @param results_file (char) path to file with FEDUP results
+#'  (must be an absolute path).
+#' @param pvalue (numeric) pvalue cutoff. Pathways with a higher pvalue
+#'  will not be included in the EM (value between 0 and 1; default 1).
+#' @param qvalue (numeric) qvalue cutoff. Pathways with a higher qvalue
+#'  will not be included in the EM (value between 0 and 1; default 1).
+#' @param net_name (char) name for EM in Cytoscape (default generic).
+#' @param net_file (char) name of output image. Supports png, pdf, svg,
+#'  jpeg image formats.
+#' @return file name of image to which the network is exported. Also side
+#'  effect of plotting the EM in an open session of Cytoscape.
 #' @examples
 #' \dontrun{
-#' # not run because this function requires Cytoscape to be installed and open
-#' data(testGene)
-#' data(backgroundGene)
-#' data(pathwaysGMT)
-#' gmt_file <- tempfile("pathwaysGMT", fileext = ".gmt")
-#' writePathways(pathwaysGMT, gmt_file)
-#' fedup_res <- runFedup(testGene, backgroundGene, pathwaysGMT)
-#' results_file <- tempfile("fedup_res", fileext = ".txt")
-#' writeFemap(fedup_res, results_file)
-#' plotFemap(
-#'		gmt_file = gmt_file,
-#'		results_file = results_file,
-#'		qvalue = 0.05,
-#'		net_name = "FEDUP_EM"
-#' )}
+#'  # not run because Cytoscape needs to be installed and open
+#'  data(testGene)
+#'  data(backgroundGene)
+#'  data(pathwaysGMT)
+#'  gmt_file <- tempfile("pathwaysGMT", fileext = ".gmt")
+#'  fedup_res <- runFedup(testGene, backgroundGene, pathwaysGMT)
+#'  results_file <- tempfile("fedup_res", fileext = ".txt")
+#'  net_file <- tempfile("FEDUP_EM", fileext = ".png")
+#'  writePathways(pathwaysGMT, gmt_file)
+#'  writeFemap(fedup_res, results_file)
+#'  plotFemap(
+#'    gmt_file = gmt_file,
+#'    results_file = results_file,
+#'    qvalue = 0.05,
+#'    net_name = "FEDUP_EM",
+#'    net_file = net_file)}
 #' @import RCy3
 #' @export
-plotFemap <- function(gmt_file,
-											results_file,
-											pvalue = 1,
-											qvalue = 1,
-							  			net_name = "generic") {
+plotFemap <- function(gmt_file, results_file,
+                        pvalue = 1, qvalue = 1,
+                        net_name = "generic", net_file = "png") {
+    # Confirm that Cytoscape is installed and opened
+    cytoscapePing()
+    if (net_name %in% getNetworkList()) {
+        deleteNetwork(net_name)
+    }
 
-	# Confirm that Cytoscape is installed and opened
-	cytoscapePing()
+    message("Building the network")
+    em_command <- paste(
+        'enrichmentmap build analysisType="generic"',
+        "gmtFile=", gmt_file,
+        "enrichmentsDataset1=", results_file,
+        "pvalue=", pvalue,
+        "qvalue=", qvalue,
+        "similaritycutoff=", 0.375,
+        "coefficients=", "COMBINED",
+        "combinedConstant=", 0.5)
+    response <- commandsGET(em_command)
+    renameNetwork(net_name, getNetworkSuid())
 
-  # Generate EM using given parameters
-	if (net_name %in% getNetworkList()) {
-		deleteNetwork(net_name)
-	}
+    # Node visualization (enriched = red nodes, depleted = blue nodes)
+    message("Setting network chart data")
+    ch_command <- paste(
+        'enrichmentmap chart data="NES_VALUE"',
+        "colors=", "RD_BU_9")
+    response <- commandsGET(ch_command)
 
-	cat("* Building the network\n")
-	em_command <- paste(
-			'enrichmentmap build analysisType="generic"',
-			"gmtFile=", gmt_file,
-			"enrichmentsDataset1=", results_file,
-			"pvalue=", pvalue,
-			"qvalue=", qvalue,
-			"similaritycutoff=", 0.375,
-			"coefficients=", "COMBINED",
-		  "combinedConstant=", 0.5)
-  response <- commandsGET(em_command)
-	renameNetwork(net_name, getNetworkSuid())
+    # Annotate similar pathways using AutoAnnotate
+    message("Annotating the network using AutoAnnotate")
+    aa_command <- paste(
+        "autoannotate annotate-clusterBoosted",
+        "clusterAlgorithm=MCL",
+        "maxWords=3",
+        "network=", net_name)
+    response <- commandsGET(aa_command)
 
-  cat("* Setting chart data\n")
-  # Node visualization (enriched = red nodes, depleted = blue nodes)
-  ch_command <- paste('enrichmentmap chart data="NES_VALUE"',
-      "colors=", "RD_BU_9")
-  response <- commandsGET(ch_command)
+    # Network layout
+    message("Applying a force-directed network layout")
+    ln_command <- paste(
+        "layout force-directed",
+        "network=", net_name)
+    response <- commandsGET(ln_command)
+    fitContent()
 
- 	# Annotate similar pathways using AutoAnnotate
-	cat("* Annotating the network using AutoAnnotate\n")
-  aa_command <- paste(
-		"autoannotate annotate-clusterBoosted",
-		"clusterAlgorithm=MCL",
-		"maxWords=3",
-		"network=", net_name
-	)
-	response <- commandsGET(aa_command)
-
-  # Network layout
-  cat("* Applying network layout\n")
-  ln_command <- paste(
-    "layout force-directed",
-    "network=", net_name
-  )
-  response <- commandsGET(ln_command)
-	fitContent()
+    # Draw out network to file
+    message("Drawing out network to ", net_file)
+    exportImage(net_file)
 }

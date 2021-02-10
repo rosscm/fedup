c0a1d53ef1e19e919f1c5e2ad45910a9528789d7
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-02-10 02:12:19 -0500

    optimizing function, concat redundant lines
    Former-commit-id: c3c63b287d86cc3cc8c2ca14170a712712964b22

--- R/femap.R
+++ R/femap.R
@@ -6,13 +6,14 @@
 #' @return table of gene enrichment and depletion results formatted as a
 #' 'Generic results file'. Rows represent tested pathways. Columns represent:
 #' \itemize{
-#'    \item pathway -- pathway ID (must match pathway IDs in the GMT file provided to plotFemap());
-#'    \item description -- gene-set name or description;
-#'    \item pvalue -- enrichment pvalue;
-#'    \item qvalue -- BH-corrected pvalue;
-#'    \item phenotype -- +1 or -1, to identify enrichment in up- and down-regulation,
-#'            or, more in general in either of the two phenotypes being compared
-#'            in the two-class analysis (+1 maps to red, -1 maps to blue)
+#'     \item pathway -- pathway ID (must match pathway IDs in the GMT file
+#'         provided to plotFemap());
+#'     \item description -- pathway name or description;
+#'     \item pvalue -- enrichment pvalue;
+#'     \item qvalue -- BH-corrected pvalue;
+#'     \item status -- +1 or -1, to identify enrichment in either of the two
+#'         phenotypes being compared in the two-class analysis
+#'         (+1 maps to red, -1 maps to blue)
 #' }
 #' @examples
 #' data(testGene)
@@ -26,11 +27,10 @@
 #' @export
 writeFemap <- function(df, results_file) {
     df_em <- df %>%
-        select("pathway", "pvalue", "qvalue", "enrichment") %>%
+        select("pathway", "pvalue", "qvalue", "status") %>%
         mutate("description" = gsub("\\%.*", "", df$pathway)) %>%
-        mutate("enrichment" = ifelse(df$enrichment == "Enriched", "1", "-1")) %>%
-        select("pathway", "description", "pvalue", "qvalue", "enrichment") %>%
-        `colnames<-` (c("pathway", "description", "pvalue", "qvalue", "phenotype"))
+        mutate("status" = ifelse(df$status == "Enriched", "1", "-1")) %>%
+        select("pathway", "description", "pvalue", "qvalue", "status")
 
     fwrite(df_em, results_file, sep = "\t", col.names = TRUE, quote = FALSE)
     message("Wrote Cytoscape-formatted FEDUP results file to ", results_file)
@@ -53,22 +53,22 @@ writeFemap <- function(df, results_file) {
 #'  effect of plotting the EM in an open session of Cytoscape.
 #' @examples
 #' \dontrun{
-#'  # not run because Cytoscape needs to be installed and open
-#'  data(testGene)
-#'  data(backgroundGene)
-#'  data(pathwaysGMT)
-#'  gmt_file <- tempfile("pathwaysGMT", fileext = ".gmt")
-#'  fedup_res <- runFedup(testGene, backgroundGene, pathwaysGMT)
-#'  results_file <- tempfile("fedup_res", fileext = ".txt")
-#'  net_file <- tempfile("FEDUP_EM", fileext = ".png")
-#'  writePathways(pathwaysGMT, gmt_file)
-#'  writeFemap(fedup_res, results_file)
-#'  plotFemap(
-#'    gmt_file = gmt_file,
-#'    results_file = results_file,
-#'    qvalue = 0.05,
-#'    net_name = "FEDUP_EM",
-#'    net_file = net_file)}
+#'     # not run because Cytoscape needs to be installed and open
+#'     data(testGene)
+#'     data(backgroundGene)
+#'     data(pathwaysGMT)
+#'     gmt_file <- tempfile("pathwaysGMT", fileext = ".gmt")
+#'     fedup_res <- runFedup(testGene, backgroundGene, pathwaysGMT)
+#'     results_file <- tempfile("fedup_res", fileext = ".txt")
+#'     net_file <- tempfile("FEDUP_EM", fileext = ".png")
+#'     writePathways(pathwaysGMT, gmt_file)
+#'     writeFemap(fedup_res, results_file)
+#'     plotFemap(
+#'         gmt_file = gmt_file,
+#'         results_file = results_file,
+#'         qvalue = 0.05,
+#'         net_name = "FEDUP_EM",
+#'         net_file = net_file)}
 #' @import RCy3
 #' @export
 plotFemap <- function(gmt_file, results_file,

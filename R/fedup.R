5b262ff48510e4502ec0f530abdaf28f57fd4c81
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-02-10 02:12:19 -0500

    optimizing function, concat redundant lines
    Former-commit-id: 3b2a7f53e126752a9a5e6c42b267c19ac3cde29d

--- R/fedup.R
+++ R/fedup.R
@@ -1,33 +1,26 @@
 inputObject <- function(test_gene, background_gene, pathways) {
-    # Stop if test_gene is empty
     if (is.null(test_gene)) {
         stop("Oops, argument 'test_gene' is empty. Supply a vector of genes
         ...I promise this will work.")
     }
 
-    # Stop if background_gene is empty
     if (is.null(background_gene)) {
         stop("Oops, argument 'background_gene' is empty. Supply a vector of
         genes ... I promise this will work")
     }
 
-    # Stop if pathways is empty or not in list format
     if (!is.list(pathways)) {
         stop("Oops, argument 'pathways' is not in a list format...
-        have you tried using readPathways()?")
+        have you tried using readPathways() on your input pathway file?")
     }
 
-    # Stop if length of test_gene is greater than background_gene
     if (length(test_gene) >= length(background_gene)) {
         stop("Oops, your test set can't have more genes than your background
         set. Have you mixed up the 'test_gene' and 'background_gene' arguments?
         You're so close... I can feel it.")
     }
 
-    # Vector of all unique genes in pathways
     pathway_genes <- unique(as.character(unlist(pathways)))
-
-    # Stop if no genes from test_gene found in pathways
     test_gene_in_pathways <- which(test_gene %in% pathway_genes)
     if (!length(test_gene_in_pathways)) {
         stop("Oops, none of the genes in 'test_gene' was found in 'pathways'.
@@ -35,21 +28,17 @@ inputObject <- function(test_gene, background_gene, pathways) {
         you expect this works?")
     }
 
-    # Stop if no genes from background_gene found in pathways
-    background_gene_in_pathways <- length(which(background_gene %in% pathway_genes))
-    if (!length(background_gene_in_pathways)) {
+    back_gene_in_pathways <- length(which(background_gene %in% pathway_genes))
+    if (!length(back_gene_in_pathways)) {
         stop("Oops, none of the genes in 'background_genes' was found in
         'pathways'. Make sure that you have some gene IDs in both inputs,
         otherwise how do you expect this works?")
     }
 
-    # Prepare input arguments
     test_gene <- unique(as.character(test_gene))
     background_gene <- unique(as.character(background_gene))
 
-    # Store in list
-    list(
-        test_gene = test_gene,
+    list(test_gene = test_gene,
         background_gene = background_gene,
         pathways = pathways,
         pathways_name = names(pathways),
@@ -59,27 +48,26 @@ inputObject <- function(test_gene, background_gene, pathways) {
 
 #' Runs gene enrichment and depletion analysis for a list of pathways.
 #'
-#' @param test_gene (char) vector of genes to test set.
+#' @param test_gene (char) vector of genes to use as test set.
 #' @param background_gene (char) vector of genes to use as background set.
 #' @param pathways (list) list of vectors with pathway annotations.
 #' @return table of pathway enrichment and depletion results. Rows represent
 #' tested pathways. Columns represent:
 #' \itemize{
-#'  \item pathway -- name of the pathway, corresponds to names(pathways);
-#'  \item size -- size of the pathway;
-#'  \item real_total -- number of genes in `test_gene`;
-#'  \item real_pathway -- number of `test_gene` members in pathway;
-#'  \item real_pathway_frac -- fraction of `test_gene` members in pathway,
-#'      evaluates as (`real_pathawy` / `real_total`) * 100;
-#'  \item expected_total -- number of genes in 'background_gene';
-#'  \item expected_pathway -- number of `background_gene` members in pathway;
-#'  \item expected_pathway_frac -- fraction of `background_gene` members in
-#'      pathway, evaluates as (`expected_pathway` / `expected_total`) * 100;
-#'  \item enrichment -- indicator that pathway is enriched or depleted for
-#'      `test_gene` members;
-#'  \item real_pathway_gene -- vector of `real_pathway` genes;
-#'  \item pvalue -- enrichment p-value calculated via Fisher's exact test;
-#'  \item qvalue -- BH-adjusted p-value
+#'     \item pathway -- name of the pathway, corresponds to
+#'         names(\code{pathways});
+#'     \item size -- size of the pathway;
+#'     \item real_frac -- fraction of \code{test_gene} members in pathway;
+#'     \item expected_frac -- fraction of \code{background_gene} members in
+#'         pathway;
+#'     \item fold_enrichment -- fold enrichment measure,
+#'         evaluates as \code{real_frac} / \code{expected_frac};
+#'     \item status -- indicator that pathway is enriched or depleted for
+#'         \code{test_gene} members;
+#'     \item real_gene -- vector of \code{test_gene} gene members annotated
+#'         to \code{pathways};
+#'     \item pvalue -- enrichment p-value calculated via Fisher's exact test;
+#'     \item qvalue -- BH-adjusted p-value
 #' }
 #' @examples
 #' data(testGene)
@@ -91,7 +79,6 @@ inputObject <- function(test_gene, background_gene, pathways) {
 #' @importFrom stats fisher.test p.adjust
 #' @export
 runFedup <- function(test_gene, background_gene, pathways) {
-    # Get input arguments
     inputs <- inputObject(test_gene, background_gene, pathways)
     test <- inputs$test_gene
     background <- inputs$background_gene
@@ -101,79 +88,43 @@ runFedup <- function(test_gene, background_gene, pathways) {
     message("Data input:\n => ",
         length(test), " test genes\n => ",
         length(background), " background genes\n => ",
-        length(pathways), " pathawys"
-    )
+        length(pathways), " pathawys")
 
     if (!length(pathways)) {
     return(data.table(
-            pathway = character(),
-            size = integer(),
-            real_total = numeric(),
-            real_pathway = numeric(),
-            real_pathway_frac = numeric(),
-            expected_total = numeric(),
-            expected_pathway = numeric(),
-            expected_pathway_frac = numeric(),
-            enrichment = character(),
-            real_pathway_gene = list(),
-            pvalue = numeric(),
-            qvalue = numeric()))
+        pathway = character(), size = integer(),
+        real_frac = numeric(), expected_frac = numeric(),
+        fold_enrichment = numeric(), status = character(),
+        real_gene = list(), pvalue = numeric(), qvalue = numeric()))
     }
 
-    # Results table
     res <- data.table(pathway = pathways_name, size = pathways_size)
-    res_stats <- sapply(pathways, function(x) {
-    # Number of test/background genes
-    a_n <- length(test)
-    b_n <- length(background)
+    res_stats <- vapply(pathways, function(x) {
+        a_n <- length(test) # n test genes
+        b_n <- length(background) # n background genes
+        a <- intersect(test, x) # test genes in pathway
+        b <- intersect(background, x) # background genes in pathway
+        a_len <- length(a) # n test genes in pathway
+        b_len <- length(b) # n background genes in pathway
+        a_x <- (a_len / a_n) * 100 # fraction of test genes in pathway
+        b_x <- (b_len / b_n) * 100 # fraction of background genes in pathway
+        f <- a_x / b_x # fold enrichment measure
+        e <- ifelse(f > 1, "Enriched", "Depleted")
+        m <- rbind(c(a_len, b_len), c(a_n, b_n)) # pval contingency table
+        p <- fisher.test(m, alternative = "two.sided")$p.value
+        return(c(
+            real_frac = a_x, expected_frac = b_x, fold_enrich = f,
+            status = e, pvalue = p, real_gene = paste(a, collapse = "|")))
+    }, character(6))
 
-    # Number of test/background genes in pathway
-    a <- intersect(test, x)
-    b <- intersect(background, x)
-    a_len <- length(a)
-    b_len <- length(b)
-
-    # Fraction of test/background genes in pathway
-    a_x <- (a_len / a_n) * 100
-    b_x <- (b_len / b_n) * 100
-
-    # Pathway enriched or depleted for test genes
-    e <- ifelse(a_x > b_x, "Enriched", "Depleted")
-
-    # Set up contingency table for p value calculation
-    m <- matrix(ncol = 2, nrow = 2)
-    m[1,] <- c(a_len, b_len)
-    m[2,] <- c(a_n, b_n)
-
-    # Calculate p value using fishers exact test
-    p <- fisher.test(m, alternative = "two.sided")$p.value
-
-    # Return
-    return(list(real_num = a_len,
-                real_frac = a_x,
-                expected_num = b_len,
-                expected_frac = b_x,
-                enrichment = e,
-                pvalue = p,
-                real_pathway_gene = a))
-    })
-
-    # Populate res table
-    res[, "real_total" := length(test)]
-    res[, "real_pathway" := unlist(res_stats["real_num",])]
-    res[, "real_pathway_frac" := unlist(res_stats["real_frac",])]
-    res[, "expected_total" := length(background)]
-    res[, "expected_pathway" := unlist(res_stats["expected_num",])]
-    res[, "expected_pathway_frac" := unlist(res_stats["expected_frac",])]
-    res[, "enrichment" := unlist(res_stats["enrichment",])]
-    res[, "real_pathway_gene" := mapply("[", res_stats["real_pathway_gene",])]
-    res[, "pvalue" := unlist(res_stats["pvalue",])]
-
-    # BH-correct p values
-    res <- res[order(res$pvalue),]
+    res[, "real_frac" := as.numeric(unlist(res_stats["real_frac",]))]
+    res[, "expected_frac" := as.numeric(unlist(res_stats["expected_frac",]))]
+    res[, "fold_enrichment" := as.numeric(unlist(res_stats["fold_enrich",]))]
+    res[, "status" := unlist(res_stats["status",])]
+    res[, "real_gene" := mapply("[", strsplit(res_stats["real_gene",], "\\|"))]
+    res[, "pvalue" := as.numeric(unlist(res_stats["pvalue",]))]
+    res <- res[order(res$pvalue),] # BH-correct pvalues
     res$qvalue <- p.adjust(res$pvalue, method = "BH")
     message("You did it! FEDUP ran successfully, feeling pretty good huh?")
-
-    # Return table
     return(res)
 }
--- R/pathways.R
+++ R/pathways.R
@@ -2,111 +2,78 @@
 #' Currently supports the following file format: GMT, TXT, XLSX.
 #'
 #' @param pathway_file (char) path to file with pathway annotations.
-#' @param header (logical) whether pathway_file has a header (default TRUE).
+#' @param header (logical) whether \code{pathway_file} has a header
+#'     (default FALSE).
 #' @param pathway_col (char) column name with pathway identifiers.
-#'  For use with for non-GMT input files (eg "Pathway.ID"; default NULL).
+#'     For use with non-GMT input files (eg "Pathway.ID"; default NULL).
 #' @param gene_col (char) column name with gene identifiers.
-#'  For use with for non-GMT input files (eg "Gene.ID"; default NULL).
+#'     For use with non-GMT input files (eg "Gene.ID"; default NULL).
 #' @param MIN_GENE (integer) minimum number of genes to be considered
-#'  in a pathway (default = 1).
+#'     in a pathway (default = 1).
 #' @param MAX_GENE (integer) maximum number of genes to be considered
-#'  in a pathway (default = Inf).
-#' @param GO_class (char) if GO terms are present in `pathway_file`, which
-#'  class(es) should be subsetted (BP, MF and/or CC, default NULL).
+#'     in a pathway (default = Inf).
+#' @param GO_class (char) if GO terms are present in \code{pathway_file},
+#'     which class(es) should be subsetted (BP, MF and/or CC, default NULL).
 #' @return a list of vectors with pathway annotations.
 #' @examples
 #' pathways <- readPathways(
-#'  system.file("extdata", "Human_Reactome_November_17_2020_symbol.gmt",
-#'  package = "FEDUP"), MIN_GENE = 10, MAX_GENE = 500)
+#'     system.file("extdata", "Human_Reactome_November_17_2020_symbol.gmt",
+#'     package = "FEDUP"), MIN_GENE = 10, MAX_GENE = 500)
 #' pathways <- readPathways(
-#'  system.file("extdata", "SAFE_terms.xlsx", package = "FEDUP"),
-#'  header = TRUE, pathway_col = "Enriched.GO.names", gene_col = "Gene.ID",
-#'  MIN_GENE = 10, MAX_GENE = 500)
+#'     system.file("extdata", "SAFE_terms.xlsx", package = "FEDUP"),
+#'     header = TRUE, pathway_col = "Enriched.GO.names", gene_col = "Gene.ID")
 #' @importFrom openxlsx read.xlsx
 #' @importFrom tibble deframe
 #' @importFrom stats aggregate na.omit
 #' @importFrom utils head read.delim tail
 #' @export
-readPathways <- function(pathway_file, header = TRUE,
+readPathways <- function(pathway_file, header = FALSE,
                         pathway_col = NULL, gene_col = NULL,
-                        MIN_GENE = 1L, MAX_GENE = Inf,
-                        GO_class = NULL) {
-    # Vector of supported file types
-    supported_types <- c("gmt", "txt", "xlsx")
-    file_type <- sub(".*\\.", "", pathway_file) # grab pathway_file extension
+                        MIN_GENE = 1L, MAX_GENE = Inf, GO_class = NULL) {
 
-    # Stop if pathway_file input is not supported
-    if (!file_type %in% supported_types) {
-    stop(paste0("Sorry, pathway file type (", file_type, ") is not supported. ",
-                "Please use one of the following formats: ",
-                paste(supported_types, collapse = ", "), "."))
-    } else {
-        message("Pathway file: ", basename(pathway_file))
-        if (file_type == "gmt") {
-            pathway_in <- strsplit(readLines(pathway_file), "\t")
-            if (header) { pathway_in <- pathway_in[-1] } # strip header if TRUE
-            pathways <- lapply(pathway_in, tail, -2)
-            names(pathways) <- sapply(pathway_in, head, 1)
-        } else {
-            if (file_type == "xlsx") {
-                pathway_in <- read.xlsx(pathway_file)
-            }
-            if (file_type == "txt")  {
-                pathway_in <- read.delim(pathway_file, header = header)
-            }
-            if (missing(pathway_col) || !pathway_col %in% colnames(pathway_in)) {
-                stop("Pathway ID column (", pathway_col, ") not in pathway file")
-            }
-            if (missing(gene_col) || !gene_col %in% colnames(pathway_in)) {
-                stop("Gene ID column (", gene_col, ") not in pathway file")
-            }
-
-            # Generate pathway list from dataframe
-            pathway_df <- data.frame(
-                pathway = pathway_in[,pathway_col],
-                gene = pathway_in[,gene_col]
-            )
-
-            # Ensure no empty entries
-            pathway_df[which(pathway_df$gene == ""), "gene"] <- NA
-            pathway_df <- na.omit(pathway_df)
-
-            # Transform to list
-            pathway_df <- aggregate(gene ~ pathway, data = pathway_df, FUN = paste)
-            pathways <- deframe(pathway_df)
-        }
-
-        # Subset for pathways within MIN_GENE, MAX_GENE range
-        pathway_size <- lapply(pathways, length)
-        pathways_sub <- pathways[which(pathway_size >= MIN_GENE & pathway_size <= MAX_GENE)]
-        message(" => n total pathway entries: ", length(pathways))
-        message(" => n pathway entries (min ", MIN_GENE,  ", max ",
-                MAX_GENE, "): ", length(pathways_sub))
-
-        # Subset for chosen 'GO_class' if specified
-        if (!is.null(GO_class)) {
-            go_keep <- paste(GO_class, collapse = "|")
-            int_keep <- grep(go_keep, names(pathways_sub))
-            pathways_sub <- pathways_sub[int_keep]
-            message(" => n pathways filtering for ", GO_class, "GO class: ",
-                length(pathways_sub))
-        }
-
-        # Finally, check for any duplicated pathway annotations
-        pathways_dup <- which(duplicated(names(pathways_sub)))
-        if (length(pathways_dup)) {
-            pathways_sub <- pathways_sub[-pathways_dup]
-            message(" => n pathways after removing duplicated entries: ",
-                length(pathways_sub))
-        }
-
-        # Stop if no pathways left after filtering
-        if (!length(pathways_sub)) {
-            stop("Oops no pathways left... try out different filtering options.")
-        }
+    s <- c("gmt", "txt", "xlsx") # supported file extensions
+    f <- sub(".*\\.", "", pathway_file) # pathway_file extension
+    if (!f %in% s) {
+        stop(paste0("Sorry, pathway file type (", f, ") is not supported. ",
+            "Supported extensions: ", paste(s, collapse = ", "), "."))
     }
-    # Return named list of pathways
-    return(pathways_sub)
+    message("Pathway file: ", basename(pathway_file))
+    if (f == "gmt") {
+        pathway_in <- strsplit(readLines(pathway_file), "\t")
+        if (header) { pathway_in <- pathway_in[-1] }
+        pathways <- lapply(pathway_in, tail, -2)
+        names(pathways) <- vapply(pathway_in, head, n = 1, character(1))
+    } else {
+        if (f == "xlsx") { pathway_in <- read.xlsx(pathway_file) }
+        if (f == "txt")  { pathway_in <- read.delim(pathway_file, header) }
+        if (missing(pathway_col)||!pathway_col %in% colnames(pathway_in)) {
+            stop("Pathway ID column (", pathway_col, ") not in file")
+        }
+        if (missing(gene_col)||!gene_col %in% colnames(pathway_in)) {
+            stop("Gene ID column (", gene_col, ") not in file")
+        }
+        pathway_df <- data.frame(
+            pathway = pathway_in[,pathway_col],
+            gene = pathway_in[,gene_col])
+        pathway_df[which(pathway_df$gene == ""), "gene"] <- NA
+        pathway_df <- na.omit(pathway_df) # ensure no NaNs
+        pathway_df <- aggregate(gene ~ pathway, pathway_df, paste)
+        pathways <- deframe(pathway_df) # transform df to list
+    }
+    size <- lapply(pathways, length) # subset for pathways in [MIN:MAX] range
+    pathways_s <- pathways[which(size >= MIN_GENE & size <= MAX_GENE)]
+    pathways_s <- pathways_s[!duplicated(names(pathways_s))]
+    message(" => n total pathways: ", length(pathways))
+    message(" => n pathways (",MIN_GENE,"-",MAX_GENE, "): ", length(pathways_s))
+    if (!is.null(GO_class)) {
+        go_keep <- grep(paste(GO_class, collapse = "|"), names(pathways_s))
+        pathways_s <- pathways_s[go_keep]
+        message(" => n pathways in", GO_class, "GO class: ", length(pathways_s))
+    }
+    if (!length(pathways_s)) {
+        stop("Oops, no pathways left... try different filtering options.")
+    }
+    return(pathways_s)
 }
 
 #' Writes a set of pathways (list of vectors) to a GMT file.
@@ -115,9 +82,9 @@ readPathways <- function(pathway_file, header = TRUE,
 #' @param gmt_file (char) name of output GMT file.
 #' @return GMT-formatted file. Rows represent pathways. Columns represent:
 #' \itemize{
-#'  \item pathway ID;
-#'  \item description;
-#'  \item a list of tab-delimited genes
+#'     \item pathway ID;
+#'     \item description;
+#'     \item a list of tab-delimited genes
 #' }
 #' @examples
 #' data(pathwaysXLSX)

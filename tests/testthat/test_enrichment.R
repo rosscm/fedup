b96cb6b0893b24235c9adc5e0cedca069ba3cd8f
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-02-11 01:52:02 -0500

    increasing test coverage
    Former-commit-id: 020b24bc4d731f62d0e735573123829bf108b3ff

--- tests/testthat/test_enrichment.R
+++ tests/testthat/test_enrichment.R
@@ -1,11 +1,27 @@
 context("Enrichment analysis")
 
+test_that("Test that FEDUP stops without proper inputs", {
+    data(testGene)
+    data(backgroundGene)
+    data(pathwaysGMT)
+
+    expect_error(runFedup(NULL, backgroundGene, pathwaysGMT))
+    expect_error(runFedup(testGene, NULL, pathwaysGMT))
+    expect_error(runFedup(testGene, backgroundGene, NULL))
+    expect_error(runFedup("123$", backgroundGene, pathwaysGMT))
+    expect_error(runFedup(testGene, "123$", pathwaysGMT))
+    expect_error(runFedup(backgroundGene, testGene, pathwaysGMT))
+})
+
 test_that("Test that FEDUP analysis works", {
   data(testGene)
   data(backgroundGene)
   data(pathwaysGMT)
-  fedup_res <- runFedup(testGene, backgroundGene, pathwaysGMT)
 
+  expect_false(length(testGene) > length(backgroundGene))
+  expect_true(is.list(pathwaysGMT))
+
+  fedup_res <- runFedup(testGene, backgroundGene, pathwaysGMT)
   expect_equal(fedup_res[1, real_frac], 100.00000)
   expect_equal(fedup_res[1, qvalue], 1.567426e-186)
   expect_true("NKX2-5" %in% fedup_res[,real_gene][[1]])

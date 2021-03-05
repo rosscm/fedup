bb0e9394cb66412a6f6ea5fdbf674ac727aa2382
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-03-05 17:56:49 -0500

    moving scripts

---
+++ inst/script/pathwaysTXT.R
@@ -0,0 +1,33 @@
+## code to prepare `pathwaysTXT` dataset goes here
+# Download from https://boonelab.ccbr.utoronto.ca/supplement/costanzo2016/
+# Data File S5 sheet 3
+library(openxlsx)
+library(tibble)
+library(biomaRt)
+library(dplyr)
+
+# Raw data file
+pathwayFile <- system.file("extdata", "Data_File_S5_SAFE_analysis_Gene_cluster_identity_and_functional_enrichments.xlsx", package = "fedup")
+pathway <- read.xlsx(pathwayFile, sheet = 3)
+
+# Query Ensembl for gene symbols annotated to SAFE terms
+ensembl <- useMart("ensembl", dataset = "scerevisiae_gene_ensembl")
+ensembl_gene <- getBM(
+    attributes = c("go_id", "ensembl_gene_id", "external_gene_name"),
+    mart = ensembl
+)
+colnames(ensembl_gene) <- c("Enriched.GO.IDs", "ORF.ID", "Gene.ID")
+pathway <- left_join(pathway, ensembl_gene, by = "Enriched.GO.IDs")
+write.table(pathway, file.path("inst", "extdata", "SAFE_terms.txt"), quote = FALSE, sep = "\t")
+
+# Raw data file annotated with gene symbols
+pathwayFile <- system.file("extdata", "SAFE_terms.txt", package = "fedup")
+pathwaysTXT <- readPathways(
+    pathwayFile,
+    header = TRUE,
+    pathCol = "Enriched.GO.names",
+    geneCol = "Gene.ID"
+)
+
+names(pathwaysTXT) <- stringi::stri_trans_general(names(pathwaysTXT), "latin-ascii")
+usethis::use_data(pathwaysTXT, compress = "xz", version = 2, overwrite = TRUE)

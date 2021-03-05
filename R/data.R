581650476b02fad5305935b6e6caf9f1fd6af7a7
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-03-05 18:01:50 -0500

    updating data scripts

--- R/data.R
+++ R/data.R
@@ -7,39 +7,49 @@
 #' system.file("extdata", "Human_Reactome_November_17_2020_symbol.gmt",
 #'     package = "fedup")
 #' Script to prepare data
-#' system.file("data-raw", "pathwaysGMT.R", package ="fedup")
+#' system.file("inst", "script", "pathwaysGMT.R", package ="fedup")
 #'
 #' @format a named list of 1437 vectors
+#' @usage data(pathwaysGMT)
 "pathwaysGMT"
 
-#' Example list of yeast SAFE terms obtained from a XLSX file.
+#' Example list of SAFE terms obtained from a XLSX file.
+#'
+#' Raw Excel data file (S5) is available from
+#' \url{https://boonelab.ccbr.utoronto.ca/supplement/costanzo2016/}
 #'
 #' Raw data location
 #' system.file("extdata", "SAFE_terms.xlsx", package = "fedup")
 #'
 #' Script to prepare data
-#' system.file("data-raw", "pathwaysXLSX.R", package = "fedup")
+#' system.file("inst", "script", "pathwaysXLSX.R", package = "fedup")
 #'
-#' @format a named list of 30 vectors
+#' @format a named list of 317 vectors
+#' @usage data(pathwaysXLSX)
 "pathwaysXLSX"
 
-#' Example list of yeast SAFE terms obtained from a TXT file.
+#' Example list of SAFE terms obtained from a TXT file.
+#'
+#' Raw Excel data file (S5) is available from
+#' \url{https://boonelab.ccbr.utoronto.ca/supplement/costanzo2016/}
 #'
 #' Raw data location
 #' system.file("extdata", "SAFE_terms.txt", package = "fedup")
 #'
 #' Script to prepare data
-#' system.file("data-raw", "pathwaysTXT.R", package = "fedup")
+#' system.file("inst", "script", "pathwaysTXT.R", package = "fedup")
 #'
-#' @format a named list of 30 vectors
+#' @format a named list of 317 vectors
+#' @usage data(pathwaysTXT)
 "pathwaysTXT"
 
 #' Example vector of human genes to use as test set for enrichment.
 #'
 #' Script to prepare data
-#' system.file("data-raw", "genes.R", package = "fedup")
+#' system.file("inst", "script", "genes.R", package = "fedup")
 #'
 #' @format a character vector with 190 elements (gene IDs)
+#' @usage data(testGene)
 "testGene"
 
 #' Example vector of human genes to use as background set for enrichment.
@@ -48,4 +58,5 @@
 #' system.file("data-raw", "genes.R", package = "fedup")
 #'
 #' @format a character vector with 10208 elements (gene IDs)
+#' @usage data(backgroundGene)
 "backgroundGene"
